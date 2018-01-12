3ca13800226bb830a9018800863c3ce691f72bda
Joe Pettit <silverp1@users.noreply.github.com>
2018-01-12 11:03:23 -0800

    Sane defaults in config.js, removed old value no longer used

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ export const ZENDESK_URI = envvar.string('ASSIGN_BOT_ZENDESK_URI');
 export const PAGERDUTY_TOKEN = envvar.string('ASSIGN_BOT_PAGERDUTY_TOKEN');
 export const PAGERDUTY_SCHEDULE_ID = envvar.string('ASSIGN_BOT_PAGERDUTY_SCHEDULE_ID');
 
-export const POLL_INTERVAL = '10s';
-export const POLL_TIMEOUT = '200s';
+export const POLL_INTERVAL = '300s';
 
-export const STARTING_TICKET_ID = 154;
+export const STARTING_TICKET_ID = 999999999; // change me!
