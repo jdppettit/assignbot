d31d4cc806200dd95ab92d00abd9285b5b3e855d
Joseph Pettit <jpettit@pettitservers.com>
2018-01-11 16:46:57 -0800

    corrected remaining issues, added better logging

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export const ZENDESK_URI = envvar.string('ASSIGN_BOT_ZENDESK_URI');
 export const PAGERDUTY_TOKEN = envvar.string('ASSIGN_BOT_PAGERDUTY_TOKEN');
 export const PAGERDUTY_SCHEDULE_ID = envvar.string('ASSIGN_BOT_PAGERDUTY_SCHEDULE_ID');
 
-export const POLL_INTERVAL = '300s';
+export const POLL_INTERVAL = '10s';
 export const POLL_TIMEOUT = '200s';
 
 export const STARTING_TICKET_ID = 154;
